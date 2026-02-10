d16c33084ad55944d2af4614c61fa6293649c63f
wikosac <wesibrani@gmail.com>
2026-02-10 12:35:16 +0700

    Change product API

--- lib/features/products/presentation/bloc/product_bloc.dart
+++ lib/features/products/presentation/bloc/product_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_midtrans/features/products/domain/entities/category.dart';
 import '../../domain/entities/product.dart';
 import '../../domain/repositories/product_repository.dart';
 
@@ -8,6 +9,10 @@ part 'product_state.dart';
 
 class ProductBloc extends Bloc<ProductEvent, ProductState> {
   final ProductRepository repository;
+  bool _hasFetched = false;
+  List<Category> _categories = [];
+
+  List<Category> get categories => _categories;
 
   ProductBloc({required this.repository}) : super(ProductInitial()) {
     on<LoadProducts>(_onLoadProducts);
@@ -32,7 +37,7 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
     Emitter<ProductState> emit,
   ) async {
     emit(ProductLoading());
-    final result = await repository.getProductsByCategory(event.categoryId);
+    final result = await repository.getProductsByCategory(event.url);
     result.fold(
       (failure) => emit(ProductError(failure.message)),
       (products) => emit(ProductLoaded(products)),
@@ -43,10 +48,11 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
     LoadCategories event,
     Emitter<ProductState> emit,
   ) async {
+    if (_hasFetched) return;
     final result = await repository.getCategories();
-    result.fold(
-      (failure) => emit(ProductError(failure.message)),
-      (categories) => emit(CategoriesLoaded(categories)),
-    );
+    result.fold((failure) => emit(ProductError(failure.message)), (categories) {
+      _categories = categories;
+      _hasFetched = true;
+    });
   }
 }
